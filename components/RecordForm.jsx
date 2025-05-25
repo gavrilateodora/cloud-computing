8192ce73f633640584afabe077df4dc826599063
gavrilateodora <gavrilateodora20@stud.ase.ro>
2025-05-25 16:16:45 +0300

    added update page for records

--- components/RecordForm.jsx
+++ components/RecordForm.jsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react'
 const RecordForm = ({entry, onSubmit}) => {
     const router = useRouter();
     const [data, setData] = useState(entry);
+    console.log(entry)
 
     const handleChange = (type, value) => {
         setData({...data, [type]: value})
@@ -23,6 +24,7 @@ const RecordForm = ({entry, onSubmit}) => {
             Name
           </label>
           <input
+            value={data.name}
             type="name"
             id="name"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -41,6 +43,7 @@ const RecordForm = ({entry, onSubmit}) => {
           <textarea
             id="message"
             rows="4"
+            value={data.description}
             onChange={(e) => handleChange("description", e.target.value)}
             className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Description placeholder"
@@ -59,7 +62,7 @@ const RecordForm = ({entry, onSubmit}) => {
             onClick={() => onSubmit(data)}
             className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
           >
-            Create
+            {entry?._id ? "Update" : "Create"}
           </button>
         </div>
       </div>
